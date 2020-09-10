2f818c993ac79e5dc0a06b756398fc3d7a8f113b
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2020-09-10 12:28:41 +0000

    Update compile options.

--- bug_3.6.1.3/build.sh
+++ bug_3.6.1.3/build.sh
@@ -2,8 +2,8 @@
 set -e
 protoc --python_out sub  --cpp_out . test.proto test_options.proto
 
-g++ -c -fPIC baz_cc_lib.cc  -o baz_cc_lib.o
-g++ -c -fPIC test_options.pb.cc  -o test_options.pb.o
-g++ -c -fPIC test.pb.cc  -o test.pb.o
+g++ --std=c++14 -c -fPIC baz_cc_lib.cc  -o baz_cc_lib.o
+g++ --std=c++14 -c -fPIC test_options.pb.cc  -o test_options.pb.o
+g++ --std=c++14 -c -fPIC test.pb.cc  -o test.pb.o
 
 g++ -shared test_options.pb.o test.pb.o baz_cc_lib.o -lprotobuf -o baz.so
